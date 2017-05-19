649035dc330c20f2588cd8471791789caadde46b
Ravikant Sharma <rkharma1401@hotmail.com>
2017-05-19 17:12:46 +0530

    update junit

---
+++ src/test/java/javaresteasydemo/TestClass.java
@@ -0,0 +1,17 @@
+package javaresteasydemo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void isValid(){
+		Assert.assertEquals(1, 1);
+	}
+	
+	@Test
+	public void isValid1(){
+		Assert.assertEquals(1, 1);
+	}
+}
